08b4005c54a54dcc17c4116cda427eb7ba18ae44
인준 <cij041109@gmail.com>
2026-04-06 10:13:11 +0900

    아기사자 과제V1 제출

--- src/week1/student/Main.java
+++ src/week1/student/Main.java
@@ -1,12 +1,41 @@
 package week1.student;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("이거 맞니");
+        int num = 0;
+        Scanner scanner = new Scanner(System.in);
+        while(true) {
+            System.out.println("저장할 아기사자 수를 5 이상 입력해주세요.");
 
+            num = scanner.nextInt();
+            scanner.nextLine();
 
-        System.out.println("따로 추가할것 menu 수정");
+            if( num <5) {
+                System.out.println("[오류] 5 이상 입력해주세요.");
+            } else {
+
+                break;
+            }
+
+        }
+        String[] lionName = new String[num];
+
+        System.out.println("아기사자 이름을 입력해주세요");
+        for (int i = 0; i<num; i++) {
+
+            lionName[i] = scanner.nextLine();
+
+        }
+
+        System.out.println("아기사자 명단을 최종적으로 출력합니다.");
+        for (int i = 0; i<num; i++) {
+
+            System.out.println((i+1) +". " + lionName[i]);
+
+        }
     }
 }
