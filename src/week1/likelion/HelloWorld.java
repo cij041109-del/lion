1159d19b611aeb3389a3063f6f6d98905cecaf2d
인준 <cij041109@gmail.com>
2026-04-07 16:29:21 +0900

    Hello, World!

---
+++ src/week1/likelion/HelloWorld.java
@@ -0,0 +1,9 @@
+package week1.likelion;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, world");
+        System.out.println("Hello, world!"); //약어 sout 입력 시 -> 자동완성
+    }
+}
