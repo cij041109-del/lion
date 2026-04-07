314347f82de2165114da085a20e487b9cfe63dd1
인준 <cij041109@gmail.com>
2026-04-07 16:29:44 +0900

    final

---
+++ src/week1/likelion/Constants.java
@@ -0,0 +1,16 @@
+package week1.likelion;
+
+public class Constants {
+    public static void main(String[] args) {
+
+
+        //상수(constants, final variables)
+        //한 번 값이 할당 => 재 할당이 불가능!!!!
+
+
+        final double PI = 3.14;
+        System.out.println(PI);
+
+       // PI = 3.141592; //에러 상수는 값 변경 불가
+    }
+}
