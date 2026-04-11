422258db7162f7106540094eaa927e282fb88bdc
인준 <cij041109@gmail.com>
2026-04-11 13:49:16 +0900

    feat: 계좌 상속 구조 구현

---
+++ src/week2/likelion/SavingAccount.java
@@ -0,0 +1,13 @@
+package week2.likelion;
+
+public class SavingAccount extends BankAccount{
+
+    boolean isOverdraft;
+    void transfer() {};
+
+
+    public void withdraw(){
+
+        System.out.println("Withdraw");
+    }
+}
---
+++ src/week2/likelion/SubscriptionAccount.java
@@ -0,0 +1,7 @@
+package week2.likelion;
+
+public class SubscriptionAccount extends BankAccount{
+
+    int numOfSubscription;
+
+}
